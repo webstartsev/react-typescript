84f4325ec21e66b82256233278a16ce76a983b62
Sergey Startsev <sergey@webstartsev.ru>
2020-03-16 14:48:00 +0300

    toogle & remove todo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import { ITodo } from './interfaces';
 
+declare var confirm: (question: string) => boolean;
+
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
 
@@ -15,12 +17,31 @@ const App: React.FC = () => {
     };
     setTodos(prev => [newTodo, ...prev]);
   };
+
+  const toggleHandler = (id: number) => {
+    setTodos(prev =>
+      prev.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    const shouldRemove = confirm(`Вы действительно хотите удалить?`);
+    if (shouldRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id));
+    }
+  };
+
   return (
     <>
       <NavBar />
       <div className="container">
         <TodoForm onAdd={addTodoHandler} />
-        <TodoList todos={todos} />
+        <TodoList todos={todos} onToggle={toggleHandler} onRemove={removeHandler} />
       </div>
     </>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,9 +3,20 @@ import { ITodo } from '../interfaces';
 
 type TodoListProps = {
   todos: ITodo[];
+  onToggle(id: number): void;
+  onRemove: (id: number) => void;
 };
 
-const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+const TodoList: React.FC<TodoListProps> = ({ todos, onRemove, onToggle }) => {
+  const removeHandler = (evt: React.MouseEvent, id: number) => {
+    evt.preventDefault();
+
+    onRemove(id);
+  };
+
+  if (!todos.length) {
+    return <p className="center">Пока дел нет</p>;
+  }
   return (
     <ul>
       {todos &&
@@ -18,9 +29,15 @@ const TodoList: React.FC<TodoListProps> = ({ todos }) => {
           return (
             <li className={classes.join(' ')} key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={onToggle.bind(null, todo.id)}
+                />
                 <span>{todo.title}</span>
-                <i className="material-icons red-text">delete</i>
+                <i className="material-icons red-text" onClick={evt => removeHandler(evt, todo.id)}>
+                  delete
+                </i>
               </label>
             </li>
           );
