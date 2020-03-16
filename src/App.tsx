900b16bd6f5692bbe94780e5608f6dc1ba9fb9f3
Sergey Startsev <sergey@webstartsev.ru>
2020-03-16 14:28:36 +0300

    add TodoList & TodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { ITodo } from './interfaces';
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addTodoHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title,
+      id: Date.now(),
+      completed: false
+    };
+    setTodos(prev => [newTodo, ...prev]);
+  };
   return (
     <>
       <NavBar />
       <div className="container">
-        <TodoForm />
+        <TodoForm onAdd={addTodoHandler} />
+        <TodoList todos={todos} />
       </div>
     </>
   );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoForm: React.FC = () => {
+interface TodoFromProps {
+  onAdd(title: string): void;
+}
+
+const TodoForm: React.FC<TodoFromProps> = props => {
+  const [title, setTitle] = useState<string>('');
+
+  const changeHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(evt.target.value);
+  };
+
+  const keyPressHandler = (evt: React.KeyboardEvent) => {
+    if (evt.key === 'Enter') {
+      props.onAdd(title);
+      setTitle('');
+    }
+  };
+
   return (
     <div className="input-field mt2">
-      <input type="text" id="title" placeholder="Введите название дела" />
+      <input
+        onChange={changeHandler}
+        value={title}
+        onKeyPress={keyPressHandler}
+        type="text"
+        id="title"
+        placeholder="Введите название дела"
+      />
       <label htmlFor="title">Введите название дела</label>
     </div>
   );
